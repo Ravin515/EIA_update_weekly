0e8190f608021edc3207dfdb68593ffc767a9482
Ravin515 <ray.stylee@outlook.com>
2023-12-10 20:54:07 +0800

    clean python code

--- Py_Version/01_eia_history.py
+++ Py_Version/01_eia_history.py
@@ -10,11 +10,22 @@ from random import randint
 # 1. 读取页面的csv文件（全量更新）
 # 筛选页面中的url
 url = 'https://www.eia.gov/petroleum/supply/weekly/archive/'
-response = ur.urlopen(url).read().decode("utf-8")
-html = etree.HTML(response)
-crawl_urls = html.xpath('//div[@class="main_col"]//a/@href')[1:]
+response = ur\
+    .urlopen(url)\
+    .read()\
+    .decode("utf-8")
 
-year_int = list(range(datetime.date.today().year - 5, datetime.date.today().year + 1, 1))
+html = etree\
+    .HTML(response)
+
+crawl_urls = html\
+    .xpath('//div[@class="main_col"]//a/@href')\
+    [1:]
+
+year_int = list(range(datetime.date.today().year - 5, 
+                    datetime.date.today().year + 1, 
+                    1))
+
 year_str = list(map(str, year_int))
 
 crawl_clean = []
@@ -37,8 +48,14 @@ def read_eia_csv(self):
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'
     }
-    data = pd.read_csv(self, encoding = "cp1252", storage_options = hdr)
-    data = data.iloc[:, :3].melt(id_vars = ["STUB_1", "STUB_2"])
+    data = pd.read_csv(self, 
+                    encoding = "cp1252", 
+                    storage_options = hdr)
+    
+    data = data\
+        .iloc[:, :3]\
+        .melt(id_vars = ["STUB_1", "STUB_2"])
+    
     return data
 
 appended_data = []
@@ -48,17 +65,29 @@ for crawl in crawl_clean:
     print("Reading " + crawl + " Success!")
 appended_data = pd.concat(appended_data)
 
-appended_data['variable'] = pd.to_datetime(appended_data['variable'], format = '%m/%d/%y').dt.strftime('%Y-%m-%d')
+appended_data['variable'] = pd\
+    .to_datetime(appended_data['variable'], format = '%m/%d/%y')\
+    .dt.strftime('%Y-%m-%d')
 
 appended_data.to_csv("eia_history.csv")
 
 # 2. 读取页面的csv文件（增量更新）
 url = 'https://www.eia.gov/petroleum/supply/weekly/archive/'
-response = ur.urlopen(url).read().decode("utf-8")
-html = etree.HTML(response)
-crawl_urls = html.xpath('//div[@class="main_col"]//a/@href')[1:]
+response = ur\
+    .urlopen(url)\
+    .read()\
+    .decode("utf-8")
 
-year_int = list(range(datetime.date.today().year, datetime.date.today().year + 1, 1))
+html = etree.HTML(response)
+
+crawl_urls = html\
+    .xpath('//div[@class="main_col"]//a/@href')\
+    [1:]
+
+year_int = list(range(datetime.date.today().year, 
+                    datetime.date.today().year + 1, 
+                    1))
+
 year_str = list(map(str, year_int))
 
 crawl_clean = []
@@ -81,8 +110,13 @@ def read_eia_csv(self):
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'
     }
-    data = pd.read_csv(self, encoding = "cp1252", storage_options = hdr)
-    data = data.iloc[:, :3].melt(id_vars = ["STUB_1", "STUB_2"])
+    data = pd.read_csv(self, 
+                    encoding = "cp1252", 
+                    storage_options = hdr)
+    data = data\
+        .iloc[:, :3]\
+        .melt(id_vars = ["STUB_1", "STUB_2"])
+    
     return data
 
 appended_data = []
@@ -92,20 +126,52 @@ for crawl in crawl_clean:
     print("Reading " + crawl + " Success!")
 appended_data = pd.concat(appended_data)
 
-appended_data['variable'] = pd.to_datetime(appended_data['variable'], format = '%m/%d/%y').dt.strftime('%Y-%m-%d')
-eia_history = pd.read_csv("eia_history.csv", encoding = "utf-8")
+appended_data['variable'] = pd\
+    .to_datetime(appended_data['variable'], format = '%m/%d/%y')\
+    .dt.strftime('%Y-%m-%d')
+
+eia_history = pd\
+    .read_csv("eia_history.csv", encoding = "utf-8")
 eia_history = pd.concat([appended_data, eia_history])
 eia_history = eia_history.drop_duplicates()
 eia_history.to_csv("eia_history.csv")
 
 # 3. 清洗出相关有用数据
-eia_history = pd.read_csv("eia_history.csv", encoding = "utf-8")
-eia_history = eia_history.drop(columns = ["Unnamed: 0", "Unnamed: 0.1"]).drop_duplicates()
-eia_history = eia_history[eia_history["STUB_2"].str.contains("Domestic Production|Percent Utilization|Finished Motor Gasoline|Distillate Fuel Oil|Kerosene-Type Jet Fuel|Residual Fuel Oil|Commercial|Cushing|SPR|Total Stocks|Crude Oil|Operable Capacity|Total Motor Gasoline") & (~eia_history["STUB_2"].str.contains("Crude Oil Inputs"))]
-eia_history = eia_history[~(eia_history["STUB_1"].str.contains("Refiner and Blender") & eia_history["STUB_2"].str.contains("Adjustment|Commercial"))] # 产量筛选
-eia_history = eia_history[~(eia_history["STUB_1"].str.contains("Stocks") & eia_history["STUB_2"].str.contains("Crude|Finished Motor Gasoline"))] # 库存筛选
-eia_history = eia_history[~(eia_history["STUB_1"].str.contains("Imports") & eia_history["STUB_2"].str.contains("Commercial|Imports by SPR|Imports into SPR by Others|Finished Motor Gasoline|Total Imports"))] # 进口筛选
-eia_history = eia_history[~(eia_history["STUB_1"].str.contains("Net Imports"))].reset_index(drop = True)
+eia_history = pd.read_csv("eia_history.csv", 
+                        encoding = "utf-8")
+eia_history = eia_history\
+    .drop(columns = ["Unnamed: 0", "Unnamed: 0.1"])\
+    .drop_duplicates()
+
+eia_history = eia_history\
+    [eia_history["STUB_2"]\
+    .str.contains("Domestic Production|Percent Utilization|Finished Motor Gasoline|Distillate Fuel Oil|Kerosene-Type Jet Fuel|Residual Fuel Oil|Commercial|Cushing|SPR|Total Stocks|Crude Oil|Operable Capacity|Total Motor Gasoline") \
+                & (~eia_history["STUB_2"]\
+                .str.contains("Crude Oil Inputs"))]
+
+eia_history = eia_history\
+    [~(eia_history["STUB_1"]\
+        .str.contains("Refiner and Blender")\
+    & eia_history["STUB_2"]\
+        .str.contains("Adjustment|Commercial"))] # 产量筛选
+
+eia_history = eia_history\
+    [~(eia_history["STUB_1"]\
+        .str.contains("Stocks")\
+    & eia_history["STUB_2"]\
+        .str.contains("Crude|Finished Motor Gasoline"))] # 库存筛选
+
+eia_history = eia_history\
+    [~(eia_history["STUB_1"]\
+        .str.contains("Imports")\
+    & eia_history["STUB_2"]\
+        .str.contains("Commercial|Imports by SPR|Imports into SPR by Others|Finished Motor Gasoline|Total Imports"))] # 进口筛选
+
+eia_history = eia_history\
+    [~(eia_history["STUB_1"]\
+        .str.contains("Net Imports"))]\
+        .reset_index(drop = True)
+
 eia_history["name"] = ["美国原油产量（千桶/天）", 
                     "美国炼厂运营炼能（千桶/天）", 
                     "美国炼厂产能利用率（%）", 
@@ -135,8 +201,13 @@ eia_history["name"] = ["美国原油产量（千桶/天）",
                     "美国汽油需求（千桶/天）",
                     "美国航煤需求（千桶/天）",
                     "美国柴油需求（千桶/天）",
-                    "美国燃料油需求（千桶/天）"]*int(len(eia_history)/30)
-eia_history["value"] = eia_history["value"].str.replace(",", "").astype("float")
+                    "美国燃料油需求（千桶/天）"]\
+                    *int(len(eia_history)/30)
+
+eia_history["value"] = eia_history["value"]\
+    .str.replace(",", "")\
+    .astype("float")
+
 eia_history["sub_title"] = np.where(
     eia_history["name"].str.contains("汽油"), "汽油",
     np.where(
--- Py_Version/03_eia_plot.py
+++ Py_Version/03_eia_plot.py
@@ -3,8 +3,13 @@ import numpy as np
 import datetime
 
 # 历史数据
-eia_history = pd.read_csv("eia_weekly.csv", encoding = "utf-8")
-eia_history = eia_history.drop(columns = ["Unnamed: 0"]).drop_duplicates()
+eia_history = pd\
+    .read_csv("eia_weekly.csv",
+            encoding = "utf-8")
+
+eia_history = eia_history\
+    .drop(columns = ["Unnamed: 0"])\
+    .drop_duplicates()
 
 # 新增数据
 def read_eia_csv(self):
@@ -16,16 +21,51 @@ def read_eia_csv(self):
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'
     }
-    data = pd.read_csv(self, encoding = "cp1252", storage_options = hdr)
-    data = data.iloc[:, :3].melt(id_vars = ["STUB_1", "STUB_2"])
+    data = pd.read_csv(self, 
+                    encoding = "cp1252",
+                    storage_options = hdr)
+    data = data\
+        .iloc[:, :3]\
+        .melt(id_vars = ["STUB_1", "STUB_2"])
+    
     return data
+
 eia_update = read_eia_csv("https://ir.eia.gov/wpsr/table9.csv")
-eia_update["variable"] = pd.to_datetime(eia_update["variable"], format = "%m/%d/%y").dt.strftime('%Y-%m-%d')
-eia_update = eia_update[eia_update["STUB_2"].str.contains("Domestic Production|Percent Utilization|Finished Motor Gasoline|Distillate Fuel Oil|Kerosene-Type Jet Fuel|Residual Fuel Oil|Commercial|Cushing|SPR|Total Stocks|Crude Oil|Operable Capacity|Total Motor Gasoline") & (~eia_update["STUB_2"].str.contains("Crude Oil Inputs"))]
-eia_update = eia_update[~(eia_update["STUB_1"].str.contains("Refiner and Blender") & eia_update["STUB_2"].str.contains("Adjustment|Commercial"))] # 产量筛选
-eia_update = eia_update[~(eia_update["STUB_1"].str.contains("Stocks") & eia_update["STUB_2"].str.contains("Crude|Finished Motor Gasoline"))] # 库存筛选
-eia_update = eia_update[~(eia_update["STUB_1"].str.contains("Imports") & eia_update["STUB_2"].str.contains("Commercial|Imports by SPR|Imports into SPR by Others|Finished Motor Gasoline|Total Imports"))] # 进口筛选
-eia_update = eia_update[~(eia_update["STUB_1"].str.contains("Net Imports"))].reset_index(drop = True)
+
+eia_update["variable"] = pd\
+    .to_datetime(eia_update["variable"],
+                format = "%m/%d/%y")\
+    .dt.strftime('%Y-%m-%d')
+
+eia_update = eia_update\
+    [eia_update["STUB_2"]\
+        .str.contains("Domestic Production|Percent Utilization|Finished Motor Gasoline|Distillate Fuel Oil|Kerosene-Type Jet Fuel|Residual Fuel Oil|Commercial|Cushing|SPR|Total Stocks|Crude Oil|Operable Capacity|Total Motor Gasoline") 
+    & (~eia_update["STUB_2"]\
+        .str.contains("Crude Oil Inputs"))]
+
+eia_update = eia_update\
+    [~(eia_update["STUB_1"]\
+        .str.contains("Refiner and Blender") 
+    & eia_update["STUB_2"]\
+        .str.contains("Adjustment|Commercial"))] # 产量筛选
+
+eia_update = eia_update\
+    [~(eia_update["STUB_1"]\
+        .str.contains("Stocks")\
+    & eia_update["STUB_2"]\
+        .str.contains("Crude|Finished Motor Gasoline"))] # 库存筛选
+
+eia_update = eia_update\
+    [~(eia_update["STUB_1"]\
+        .str.contains("Imports")
+    & eia_update["STUB_2"]\
+        .str.contains("Commercial|Imports by SPR|Imports into SPR by Others|Finished Motor Gasoline|Total Imports"))] # 进口筛选
+
+eia_update = eia_update\
+    [~(eia_update["STUB_1"]\
+        .str.contains("Net Imports"))]\
+        .reset_index(drop = True)
+
 eia_update["name"] = ["美国原油产量（千桶/天）", 
                     "美国炼厂运营炼能（千桶/天）", 
                     "美国炼厂产能利用率（%）", 
@@ -56,7 +96,11 @@ eia_update["name"] = ["美国原油产量（千桶/天）",
                     "美国航煤需求（千桶/天）",
                     "美国柴油需求（千桶/天）",
                     "美国燃料油需求（千桶/天）"]
-eia_update["value"] = eia_update["value"].str.replace(",", "").astype("float")
+
+eia_update["value"] = eia_update["value"]\
+    .str.replace(",", "")\
+    .astype("float")
+
 eia_update["sub_title"] = np.where(
     eia_update["name"].str.contains("汽油"), "汽油",
     np.where(
@@ -69,25 +113,61 @@ eia_update["sub_title"] = np.where(
         )
     )
 ) 
-eia_new = pd.concat([eia_update, eia_history], sort = True).drop_duplicates().sort_values(by = [ "sub_title", "name", "variable"]).reset_index(drop = True)
+eia_new = pd\
+    .concat([eia_update, eia_history], sort = True)\
+    .drop_duplicates()\
+    .sort_values(by = [ "sub_title", "name", "variable"])\
+    .reset_index(drop = True)
+
 eia_new.to_csv("eia_weekly.csv")
 
 # eia_new = eia_new[eia_new["name"].isnull()]
 # eia_new = eia_new.drop(["name", "sub_title"], axis = 1, inplace = True)
 
 
-eia_new["year"] = eia_new["variable"].str.slice(start = 0, stop = 4).astype("int")
-eia_new["week"] = eia_new.groupby(["name", "year"]).cumcount() + 1
-eia_new = eia_new[eia_new.year >= datetime.date.today().year-5]
-eia_new["y_max"] = eia_new[eia_new.year < max(eia_new.year)].groupby(["name", "week"])["value"].transform("max")
-eia_new["y_max"] = eia_new.groupby(["name", "week"], group_keys=False)["y_max"].apply(lambda x: x.ffill().bfill())
-eia_new["y_min"] = eia_new[eia_new.year < max(eia_new.year)].groupby(["name", "week"])["value"].transform("min")
-eia_new["y_min"] = eia_new.groupby(["name", "week"], group_keys=False)["y_min"].apply(lambda x: x.ffill().bfill())
-eia_new = eia_new[eia_new.year >= datetime.date.today().year-4]
+eia_new["year"] = eia_new["variable"]\
+    .str.slice(start = 0, stop = 4)\
+    .astype("int")
+
+eia_new["week"] = eia_new\
+    .groupby(["name", "year"])\
+    .cumcount()\
+    + 1
+
+eia_new = eia_new\
+    [eia_new.year >= datetime.date.today().year - 5]
+
+eia_new["y_max"] = eia_new\
+    [eia_new.year < max(eia_new.year)]\
+    .groupby(["name", "week"])\
+    ["value"]\
+    .transform("max")
+
+eia_new["y_max"] = eia_new\
+    .groupby(["name", "week"]
+            , group_keys=False)\
+    ["y_max"]\
+    .apply(lambda x: x.ffill().bfill())
+
+eia_new["y_min"] = eia_new\
+    [eia_new.year < max(eia_new.year)]\
+    .groupby(["name", "week"])\
+    ["value"]\
+    .transform("min")
+
+eia_new["y_min"] = eia_new\
+    .groupby(["name", "week"], group_keys=False)\
+    ["y_min"]\
+    .apply(lambda x: x.ffill().bfill())
+
+eia_new = eia_new\
+    [eia_new.year >= datetime.date.today().year-4]
+
 eia_new["y_max"] = eia_new["y_max"].astype("float")
 eia_new["y_min"] = eia_new["y_min"].astype("float")
 eia_new["value"] = eia_new["value"].astype("float")
 eia_new["week"] = eia_new["week"].astype("string")
+
 eia_new = eia_new.reset_index(drop = True)
 eia_new = eia_new.replace([np.inf, -np.inf], np.nan)
 
@@ -131,39 +211,45 @@ f, axs = plt.subplots(6, 5, figsize=(60, 30))
 axs = axs.flatten()
 for ax, name in zip(axs, names):
     eia = eia_new[(eia_new["name"] == name)]
-    sns.lineplot(ax = ax, x = eia["week"].astype("int64"), 
-                 y = 'value', 
-                 hue = eia["year"].astype("category"), 
-                 units = "year", 
-                 data = eia,
-                 palette = sns.color_palette("tab10")) # 画季节性图
+    sns.lineplot(ax = ax, 
+                x = eia["week"].astype("int64"), 
+                y = 'value', 
+                hue = eia["year"].astype("category"), 
+                units = "year", 
+                data = eia,
+                palette = sns.color_palette("tab10")) # 画季节性图
     
     sns.lineplot(x = eia["week"].astype("int64"), 
-                  y = "y_min",
-                  data = eia, 
-                  color = "grey", 
-                  alpha = 0,
-                  ax = ax)
+                y = "y_min",
+                data = eia, 
+                color = "grey", 
+                alpha = 0,
+                ax = ax)
     
     sns.lineplot(x = eia["week"].astype("int64"), 
-                  y = "y_max",
-                  data = eia, 
-                  color = "grey", 
-                  alpha = 0,
-                  ax = ax)
+                y = "y_max",
+                data = eia, 
+                color = "grey", 
+                alpha = 0,
+                ax = ax)
     
-    ax.fill_between(x = eia[["week", "y_min", "y_max"]].drop_duplicates()["week"].astype("int64"),
-                      y1 = eia[["week", "y_min", "y_max"]].drop_duplicates()['y_min'],
-                      y2 = eia[["week", "y_min", "y_max"]].drop_duplicates()['y_max'],
-                      alpha = 0.3,
-                      facecolor = 'grey',
-                      label = "5-year Range")
+    ax.fill_between(x = eia[["week", "y_min", "y_max"]]\
+                        .drop_duplicates()["week"]\
+                        .astype("int64"),
+                    y1 = eia[["week", "y_min", "y_max"]]
+                        .drop_duplicates()['y_min'],
+                    y2 = eia[["week", "y_min", "y_max"]]\
+                        .drop_duplicates()['y_max'],
+                    alpha = 0.3,
+                    facecolor = 'grey',
+                    label = "5-year Range")
     
     ax.legend(loc='upper center', 
-              bbox_to_anchor=(0.5, -0.12),
-              fancybox=True, 
-              shadow=True, 
-              ncol=5)
+                bbox_to_anchor=(0.5, -0.12),
+                fancybox=True, 
+                shadow=True, 
+                ncol=5)
+    
     ax.set_title(name, fontsize = 20)
     ax.set(xlabel = "周数")
     ax.set(ylabel = None)
@@ -177,7 +263,9 @@ eia_tab = eia_tab.resize((1600, 3000))
 # eia_tab.show()
 eia_pic = Image.open("eia_pic.png")
 # eia_pic.show()
-eia = Image.new('RGB', (eia_tab.size[0] + eia_pic.size[0], eia_tab.size[1]))
+eia = Image.new('RGB', 
+                (eia_tab.size[0] + eia_pic.size[0], 
+                    eia_tab.size[1]))
 eia.paste(eia_tab, (0, 0))
 eia.paste(eia_pic, (eia_tab.size[0], 0))
 # eia.show()
